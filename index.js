9adbf527d646b0366d0a7eb7576f3df3a80f0db2
Lawrence Gomez <lawrencemilesgomez@Lawrences-iMac.lan1>
2021-03-11 15:23:00 -0600

    added website

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const path = require('path');
 
 const app = express();
 
+// Static Middleware
+app.use(express.static(path.join(__dirname, 'cv')));
+
+app.set('cv', path.join(__dirname, 'cv'));
+app.set('cv engine', 'ejs');
+
 app.get('*', (req, res) => {
     console.log("CV triggered...");
-    res.sendFile(path.join(__dirname, './cv', 'index.html'));
+    res.render(path.join(__dirname, './cv', 'index.html'));
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 console.log('Server listening on:', PORT);
 app.listen(PORT);
